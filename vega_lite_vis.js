0b566feef6ca2ccb6d70121ee622379ff66ae43d
juneveryday <jjin0031@student.monash.edu>
2024-10-03 13:55:09 +0800

    20241003 Prepare Submit

---
+++ vega_lite_vis.js
@@ -0,0 +1,7 @@
+var vg_domestic = "1. Water consumption domestic.json"; 
+vegaEmbed("#map_chart_1", vg_domestic).then(function(result) {
+}).catch(console.error);
+
+var vg_production = "5. water_access_bubble.json"; 
+vegaEmbed("#map_chart_5", vg_production).then(function(result) {
+}).catch(console.error);
